7936bb5a0bf44cad6e776120dc8c7dae17561fcd
ANDREA CORDERO ESPINOZA <andrea.cordero.espinoza@est.una.ac.cr>
2026-04-15 20:55:50 -0600

    creación de equipo

---
+++ Equipo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andre on 15/4/2026.
+//
+
+#include "Equipo.h"
---
+++ Equipo.h
@@ -0,0 +1,17 @@
+//
+// Created by andre on 15/4/2026.
+//
+
+#ifndef PROYECTOPROGRAII_EQUIPO_H
+#define PROYECTOPROGRAII_EQUIPO_H
+
+
+class Equipo {
+    bool activo;
+public:
+    Equipo();
+    virtual ~Equipo() = 0;
+};
+
+
+#endif //PROYECTOPROGRAII_EQUIPO_H
